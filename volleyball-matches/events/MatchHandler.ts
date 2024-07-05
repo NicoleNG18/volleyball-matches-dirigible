38bb2fb3cb199af94e65ace80e29249d121d4471
admin <nicole.georgieva18@abv.bg>
2024-07-05 12:25:46 +0300

    Match handler for adding team points

--- volleyball-matches/events/MatchHandler.ts
+++ volleyball-matches/events/MatchHandler.ts
@@ -1,19 +1,30 @@
 import { MatchRepository } from "volleyball-matches/gen/volleyball-matches/dao/Matches/MatchRepository";
+import { TeamRepository } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamRepository";
 
 export const trigger = (event) => {
 
     const MatchDao = new MatchRepository();
+    const TeamDao = new TeamRepository();
+
     const matchId = event.key.value;
+    const match = MatchDao.findById(matchId);
+
+    let hostTeam = TeamDao.findById(match.Host);
+    let guestTeam = TeamDao.findById(match.Guest);
+
     const operation = event.operation;
 
     if (operation === "create") {
-        const match = MatchDao.findById(matchId);
 
-        console.log(match);
+        hostTeam.Points += match.PointsHost;
+        guestTeam.Points += match.PointsGuest;
+
+        TeamDao.update(hostTeam);
+        TeamDao.update(guestTeam);
+
+
     } else if (event.operation === "update") {
-        // TODO find by Item Id and update
     } else if (event.operation === "delete") {
-        // TODO find by Item Id and mark as deleted
     } else {
         throw new Error("Unknown operation: " + event.operation);
     }
