9f84df490236f09305569036f3ba32713d285c86
admin <nicole.georgieva18@abv.bg>
2024-07-06 16:53:49 +0300

    adding team points in the event

--- volleyball-matches/events/MatchHandler.ts
+++ volleyball-matches/events/MatchHandler.ts
@@ -1,16 +1,37 @@
 import { MatchRepository } from "volleyball-matches/gen/volleyball-matches/dao/Matches/MatchRepository";
 import { TeamRepository } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamRepository";
+import { LeagueRepository } from "volleyball-matches/gen/volleyball-matches/dao/League/LeagueRepository";
+import { TeamPointsRepository } from "volleyball-matches/gen/volleyball-matches/dao/entities/TeamPointsRepository";
 
 export const trigger = (event) => {
 
     const MatchDao = new MatchRepository();
     const TeamDao = new TeamRepository();
+    const LeagueDao = new LeagueRepository();
+    const TeamPointsDao = new TeamPointsRepository();
 
     const matchId = event.key.value;
     const match = MatchDao.findById(matchId);
 
     let hostTeam = TeamDao.findById(match.Host);
+    let hostTeamPoints = TeamPointsDao.findAll({
+        $filter: {
+            equals: {
+                Team: hostTeam.Id
+            }
+        }
+    });
+
     let guestTeam = TeamDao.findById(match.Guest);
+    let guestTeamPoints = TeamPointsDao.findAll({
+        $filter: {
+            equals: {
+                Team: guestTeam.Id
+            }
+        }
+    });
+
+    const league = LeagueDao.findById(match.League);
 
     const operation = event.operation;
 
@@ -22,9 +43,36 @@ export const trigger = (event) => {
         TeamDao.update(hostTeam);
         TeamDao.update(guestTeam);
 
+        //add points to leagues
+        if (league.Name === "VNL") {
+
+            hostTeamPoints[0].VNL += match.PointsHost;
+            guestTeamPoints[0].VNL += match.PointsGuest;
+
+        } else if (league.Name === "Olympic Games") {
+
+            hostTeamPoints[0].OlympicGames += match.PointsHost;
+            guestTeamPoints[0].OlympicGames += match.PointsGuest;
+
+        } else if (league.Name === "European Championship") {
+
+            hostTeamPoints[0].EuropeanChamp += match.PointsHost;
+            guestTeamPoints[0].EuropeanChamp += match.PointsGuest;
+
+        } else if (league.Name === "World Championship") {
+
+            hostTeamPoints[0].WorldChamp += match.PointsHost;
+            guestTeamPoints[0].WorldChamp += match.PointsGuest;
+
+        }
+
+        TeamPointsDao.update(hostTeamPoints);
+        TeamPointsDao.update(guestTeamPoints);
+
     } else if (event.operation === "update") {
     } else if (event.operation === "delete") {
     } else {
         throw new Error("Unknown operation: " + event.operation);
     }
+
 }
---
+++ volleyball-matches/gen/volleyball-matches/ui/entities/TeamPoints/dialog-filter/controller.js
@@ -0,0 +1,80 @@
+angular.module('page', ["ideUI", "ideView"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'volleyball-matches.entities.TeamPoints';
+	}])
+	.controller('PageController', ['$scope', 'messageHub', 'ViewParameters', function ($scope, messageHub, ViewParameters) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			$scope.entity = params.entity ?? {};
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+		}
+
+		$scope.filter = function () {
+			let entity = $scope.entity;
+			const filter = {
+				$filter: {
+					equals: {
+					},
+					notEquals: {
+					},
+					contains: {
+					},
+					greaterThan: {
+					},
+					greaterThanOrEqual: {
+					},
+					lessThan: {
+					},
+					lessThanOrEqual: {
+					}
+				},
+			};
+			if (entity.Id !== undefined) {
+				filter.$filter.equals.Id = entity.Id;
+			}
+			if (entity.Team !== undefined) {
+				filter.$filter.equals.Team = entity.Team;
+			}
+			if (entity.VNL !== undefined) {
+				filter.$filter.equals.VNL = entity.VNL;
+			}
+			if (entity.OlympicGames !== undefined) {
+				filter.$filter.equals.OlympicGames = entity.OlympicGames;
+			}
+			if (entity.EuropeanChamp !== undefined) {
+				filter.$filter.equals.EuropeanChamp = entity.EuropeanChamp;
+			}
+			if (entity.WorldChamp !== undefined) {
+				filter.$filter.equals.WorldChamp = entity.WorldChamp;
+			}
+			if (entity.SumPoints !== undefined) {
+				filter.$filter.equals.SumPoints = entity.SumPoints;
+			}
+			messageHub.postMessage("entitySearch", {
+				entity: entity,
+				filter: filter
+			});
+			$scope.cancel();
+		};
+
+		$scope.resetFilter = function () {
+			$scope.entity = {};
+			$scope.filter();
+		};
+
+		$scope.cancel = function () {
+			messageHub.closeDialogWindow("TeamPoints-filter");
+		};
+
+		$scope.clearErrorMessage = function () {
+			$scope.errorMessage = null;
+		};
+
+	}]);
