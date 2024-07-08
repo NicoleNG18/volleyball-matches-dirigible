c00463858445bb7cef445ca0549531d463a30c14
admin <nicole.georgieva18@abv.bg>
2024-07-08 17:22:02 +0300

    Changing the dashboard

--- volleyball-ui/api/TeamService.ts
+++ volleyball-ui/api/TeamService.ts
@@ -1,5 +1,4 @@
 import { TeamRepository as TeamDao } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamRepository";
-import { TeamPointsRepository as TeamPointsDao } from "volleyball-matches/gen/volleyball-matches/dao/entities/TeamPointsRepository";
 
 import { Controller, Get } from "sdk/http";
 import { query } from "sdk/db";
@@ -8,11 +7,9 @@ import { query } from "sdk/db";
 class TeamService {
 
     private readonly teamDao;
-    private readonly teamPointsDao;
 
     constructor() {
         this.teamDao = new TeamDao();
-        this.teamPointsDao = new TeamPointsDao();
     }
 
     @Get("/teamData")
@@ -50,7 +47,7 @@ class TeamService {
             }
         });
 
-        const sqlUnits = "SELECT t.TEAM_NAME as NAME, t.TEAM_LEAGUE AS LEAGUE, tp.TEAMPOINTS_SUMPOINTS as SUMPOINTS FROM VOLLEYBALL_MATCHES_TEAM t JOIN VOLLEYBALL_MATCHES_TEAMPOINTS tp ON t.TEAM_TEAMPOINTS=tp.TEAMPOINTS_ID ORDER BY tp.TEAMPOINTS_SUMPOINTS DESC LIMIT 5";
+        const sqlUnits = "SELECT t.TEAM_NAME as NAME, t.TEAM_LEAGUE AS LEAGUE, t.TEAM_SUMPOINTS as SUMPOINTS FROM VOLLEYBALL_MATCHES_TEAM t ORDER BY t.TEAM_SUMPOINTS DESC LIMIT 5";
         let resultset = query.execute(sqlUnits);
 
         const topFiveTeams = resultset.map(row => ({
