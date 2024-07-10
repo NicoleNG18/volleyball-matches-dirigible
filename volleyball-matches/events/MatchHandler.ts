a0bcc0edead0c228e70ac17c7455c988c8acebbd
admin <nicole.georgieva18@abv.bg>
2024-07-11 00:09:57 +0300

    Fixing match handler

--- volleyball-matches/events/MatchHandler.ts
+++ volleyball-matches/events/MatchHandler.ts
@@ -1,12 +1,14 @@
 import { MatchRepository } from "volleyball-matches/gen/volleyball-matches/dao/Matches/MatchRepository";
 import { TeamRepository } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamRepository";
 import { LeagueRepository } from "volleyball-matches/gen/volleyball-matches/dao/League/LeagueRepository";
+import { TeamLeagueRepository } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamLeagueRepository";
 
 export const trigger = (event) => {
 
     const MatchDao = new MatchRepository();
     const TeamDao = new TeamRepository();
     const LeagueDao = new LeagueRepository();
+    const TeamLeagueDao = new TeamLeagueRepository();
 
     const matchId = event.key.value;
     const match = MatchDao.findById(matchId);
@@ -14,42 +16,44 @@ export const trigger = (event) => {
     let hostTeam = TeamDao.findById(match.Host);
     let guestTeam = TeamDao.findById(match.Guest);
 
-    const league = LeagueDao.findById(match.League);
-
     const operation = event.operation;
 
     if (operation === "create") {
 
-        //add points to leagues
-        if (league.Name === "VNL") {
+        let hostTeamLeague = TeamLeagueDao.findAll({
+            $filter: {
+                equals: {
+                    Team: match.Host,
+                    League: match.League
+                }
+            }
+        });
 
-            hostTeam.VNLpoints += match.PointsHost;
-            guestTeam.VNLpoints += match.PointsGuest;
+        let guestTeamLeague = TeamLeagueDao.findAll({
+            $filter: {
+                equals: {
+                    Team: match.Guest,
+                    League: match.League
+                }
+            }
+        });
 
-        } else if (league.Name === "Olympic Games") {
+        hostTeamLeague[0].Points += match.PointsHost;
+        guestTeamLeague[0].Points += match.PointsGuest;
 
-            hostTeam.OlympicGamesPoints += match.PointsHost;
-            guestTeam.OlympicGamesPoints += match.PointsGuest;
+        hostTeam.SumPoints += match.PointsHost;
+        guestTeam.SumPoints += match.PointsGuest;
 
-        } else if (league.Name === "European Championship") {
+        TeamLeagueDao.update(hostTeamLeague);
+        TeamLeagueDao.update(guestTeamLeague);
 
-            hostTeam.EuropeanChampPoints += match.PointsHost;
-            guestTeam.EuropeanChampPoints += match.PointsGuest;
-
-        } else if (league.Name === "World Championship") {
-
-            hostTeam.WorldChampPoints += match.PointsHost;
-            guestTeam.WorldChampPoints += match.PointsGuest;
-
-        }
-
-        TeamDao.update(hostTeam);
         TeamDao.update(guestTeam);
+        TeamDao.update(hostTeam);
 
-    } else if (event.operation === "update") {
+    }
+    else if (event.operation === "update") {
     } else if (event.operation === "delete") {
     } else {
         throw new Error("Unknown operation: " + event.operation);
     }
-
 }
