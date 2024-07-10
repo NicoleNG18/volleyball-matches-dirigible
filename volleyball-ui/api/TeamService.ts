6342be365c19b49563f01672ac7218b1c1829915
admin <nicole.georgieva18@abv.bg>
2024-07-10 23:39:38 +0300

    fixing teamService methods

--- volleyball-ui/api/TeamService.ts
+++ volleyball-ui/api/TeamService.ts
@@ -1,4 +1,6 @@
 import { TeamRepository as TeamDao } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamRepository";
+import { TeamLeagueRepository as TeamLeagueDao } from "volleyball-matches/gen/volleyball-matches/dao/Teams/TeamLeagueRepository";
+
 
 import { Controller, Get } from "sdk/http";
 import { query } from "sdk/db";
@@ -7,53 +9,55 @@ import { query } from "sdk/db";
 class TeamService {
 
     private readonly teamDao;
+    private readonly teamLeagueDao;
 
     constructor() {
         this.teamDao = new TeamDao();
+        this.teamLeagueDao = new TeamLeagueDao();
     }
 
     @Get("/teamData")
     public teamData() {
 
-        let vnlTeams = this.teamDao.findAll({
+        let vnlTeams = this.teamLeagueDao.findAll({
             $filter: {
                 equals: {
                     League: 1
                 }
             }
-        }).sort((a, b) => a.VNLpoints > b.VNLpoints ? -1 : a.VNLpoints < b.VNLpoints ? 1 : 0);
+        }).sort((a, b) => a.Points > b.Points ? -1 : a.Points < b.Points ? 1 : 0);
 
-        let olympicTeams = this.teamDao.findAll({
+        let olympicTeams = this.teamLeagueDao.findAll({
             $filter: {
                 equals: {
                     League: 2
                 }
             }
-        }).sort((a, b) => a.VNLpoints > b.VNLpoints ? -1 : a.VNLpoints < b.VNLpoints ? 1 : 0);
+        }).sort((a, b) => a.Points > b.Points ? -1 : a.Points < b.Points ? 1 : 0);
 
-        let europeanChampTeams = this.teamDao.findAll({
+        let europeanChampTeams = this.teamLeagueDao.findAll({
             $filter: {
                 equals: {
                     League: 3
                 }
             }
-        }).sort((a, b) => a.VNLpoints > b.VNLpoints ? -1 : a.VNLpoints < b.VNLpoints ? 1 : 0);
+        }).sort((a, b) => a.Points > b.Points ? -1 : a.Points < b.Points ? 1 : 0);
 
-        let worldChampTeams = this.teamDao.findAll({
+        let worldChampTeams = this.teamLeagueDao.findAll({
             $filter: {
                 equals: {
                     League: 4
                 }
             }
-        }).sort((a, b) => a.VNLpoints > b.VNLpoints ? -1 : a.VNLpoints < b.VNLpoints ? 1 : 0);
+        }).sort((a, b) => a.Points > b.Points ? -1 : a.Points < b.Points ? 1 : 0);
 
-        const sqlUnits = "SELECT t.TEAM_NAME as NAME, t.TEAM_LEAGUE AS LEAGUE, t.TEAM_SUMPOINTS as SUMPOINTS FROM VOLLEYBALL_MATCHES_TEAM t ORDER BY t.TEAM_SUMPOINTS DESC LIMIT 5";
+        const sqlUnits = "SELECT t.TEAM_NAME as NAME, t.TEAM_SUMPOINTS AS POINTS,tl.TEAMLEAGUE_LEAGUE AS LEAGUE FROM VOLLEYBALL_MATCHES_TEAM t JOIN VOLLEYBALL_MATCHES_TEAMLEAGUE tl ON t.TEAM_ID=tl.TEAMLEAGUE_TEAM ORDER BY t.TEAM_SUMPOINTS DESC LIMIT 5";
         let resultset = query.execute(sqlUnits);
 
         const topFiveTeams = resultset.map(row => ({
             Name: row.NAME,
             League: row.LEAGUE,
-            Points: row.SUMPOINTS
+            Points: row.POINTS
         }));
 
         return {
